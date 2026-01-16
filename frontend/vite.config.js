8259fcdb3b38bd3f1b396f015d9477a90894c619
Sania Bushra <125661017+SaniaBushra@users.noreply.github.com>
2026-01-17 00:54:17 +0530

    Update Vite config for HTTPS and output directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://trustworthy-solace-production-7618.up.railway.app',
+        target: 'https://trustworthy-solace-production-7618.up.railway.app',
         changeOrigin: true,
       },
     },
